6386d90af2e54b36ba03dfd1202e6fcc8e16e44d
Alex Rothberg <agrothberg@gmail.com>
2016-01-13 23:08:02 -0500

    Enable circle-ci

---
+++ .circle-save-all.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p ~/docker
+
+docker save 4catalyzer/ipython > ~/docker/ipython.tar
+docker save 4catalyzer/theano > ~/docker/theano.tar
+
+docker save 4catalyzer/lasagne > ~/docker/lasagne.tar
+docker save 4catalyzer/nolearn > ~/docker/nolearn.tar
+
+docker save 4catalyzer/keras:theano > ~/docker/keras-theano.tar
+docker save 4catalyzer/keras:tf > ~/docker/keras-tf.tar
